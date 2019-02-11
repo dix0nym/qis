b900f29f9b9a99d527578003079ad9c6d379da91
dix0nym <dixonym@mailbox.org>
2019-02-11 21:38:05 +0100

    added logging

--- qis.py
+++ qis.py
@@ -1,31 +1,37 @@
+import datetime
+import json
+import logging
+import logging.config
+import os
+import pickle
+import smtplib
+import ssl
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from types import SimpleNamespace as Namespace
+
 import requests
 from bs4 import BeautifulSoup as bs
-import pickle
-import os
 from tabulate import tabulate
-import schedule
-import datetime
-import time
-import json
-from sendmail import SendMail
-from types import SimpleNamespace as Namespace
 from user_agent import generate_user_agent
 
 headers = {
     'User-Agent': generate_user_agent(),
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept': 'modul/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language':'en-US,en;q=0.5'
 }
 
+logger = logging.getLogger(__name__)
 url = "https://qis.hs-albsig.de/qisserver/rds?state=user&type=0"
-logout_url = "https://qis.hs-albsig.de/qisserver/rds?state=user&amp;type=4&amp;re=last&amp;category=auth.logout&amp;breadCrumbSource=portal&amp;topitem=functions"
+logout_url = "https://qis.hs-albsig.de/qisserver/rds?state=user&type=4&re=last&category=auth.logout&breadCrumbSource=&topitem=functions"
 login_url = 'https://qis.hs-albsig.de/qisserver/rds?state=user&type=1&category=auth.login&startpage=portal.vm'
 verwaltung = 'https://qis.hs-albsig.de/qisserver/rds?state=change&type=1&moduleParameter=studyPOSMenu&nextdir=change&next=menu.vm&subdir=applications&xml=menu&purge=y&navigationPosition=functions%2CstudyPOSMenu&breadcrumb=studyPOSMenu&topitem=functions&subitem=studyPOSMenu'
     
 class grade:
-    def __init__(self, nummer,text, semester, note, status, ects, art, pv, vs, datum):
+    def __init__(self, nummer, modul, semester, note, status, ects, art, pv, vs, datum):
         self.nummer = int(nummer)
-        self.text = text
+        self.modul = modul
         self.semester = semester
         self.note = float(note.replace(',', '.')) if note else note
         self.status = status
@@ -54,10 +60,23 @@ class grade:
         return hash(tuple(sorted(self.__dict__.items())))
     
     def __str__(self):
-        return "{} {} {} {} {} {} {}".format(self.nummer, self.text, self.semester, self.note, self.status, self.ects, self.datum)
+        return "{} {} {} {} {} {} {}".format(self.nummer, self.modul, self.semester, self.note, self.status, self.ects, self.datum)
 
     def __repr__(self):
         return self.__str__()
+
+def setup_logging(default_path='logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+    """Setup logging configuration"""
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as config_file:
+            config = json.load(config_file)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
 def parse_data(soup):
     content = soup.find('div', attrs={'class' :'content'})
@@ -83,16 +102,48 @@ def get_leistungen_url(source):
         exit("smth went wrong, couldnt find leistungen")
     return leistungen[0]['href']
 
+def notify(diff, config):
+    fulltable = tabulate([entry.get_as_list() for entry in diff], headers=["Nr", "Modul", "Semester", "Note", "Status", "ECTS", "Art", "pv", "vs", "Datum"])
+    if config.sendMail:
+        server = getMailServer(config.senderMail.username, config.senderMail.password)
+        logger.info("sent email to {}".format(config.receiveMail))
+        if not sendMail(server, config.senderMail.username, config.receiveMail, "Veränderung im QIS", fulltable):
+            logger.error("failed to sent email to {}".format(config.receiveMail))
+        for email in config.notifyEmails:
+            table = tabulate([ [entry.nummer, entry.modul] for entry in diff], headers=["Nr", "Modul"])
+            logger.info("sent email to {}".format(email))
+            if not sendMail(server, config.senderMail.username, email, "Veränderung im QIS", table):
+                logger.error("failed to sent email to {}".format(email))
+    else:
+        logger.info(fulltable)
+
+def getMailServer(username, password):
+    server = smtplib.SMTP("smtp.web.de", 587)
+    server.starttls()
+    server.login(username, password)
+    return server
+
+def sendMail(server, sender_email, receiver, subject, message):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = receiver
+        msg['Subject'] = subject
+        msg.attach(MIMEText(message, 'plain'))
+        server.sendmail(sender_email, receiver, msg.as_string())
+    except Exception:
+        return False
+    return True
 
 def check_grades(grades, config):
     session = requests.session()
     session.get(url, headers=headers)
     payload = {'asdf': config.qisLogin.username, 'submit': 'Ok', 'fdsa': config.qisLogin.password}
     resp = session.post(login_url, payload, headers=headers)
-    if logout_url not in resp.text:
-        print(resp.text)
+    if "angemeldet" not in resp.text:
+        logger.info(resp.text)
         exit("login failed")
-    print("login successfull")
+    logger.debug("login successfull")
     # navigate to leistungen
     resp = session.get(verwaltung)
     resp = session.get(get_notenspiegel_url(resp.text))
@@ -100,21 +151,20 @@ def check_grades(grades, config):
 
     new_grades = parse_data(bs(resp.text, 'html.parser'))
     if grades:
-        diff = list(set(new_grades) - set(grades))
+        diff = sorted(list(set(new_grades) - set(grades)))
         if diff:
-            table = tabulate([entry.get_as_list() for entry in diff], headers=["Nr", "Modul", "Semester", "Note", "Status", "ECTS", "Art", "pv", "vs", "Datum"])
-            if config.sendMail:
-                print("sent email to {}".format(config.receiveMail))
-                s = SendMail(config.senderMail.username, config.senderMail.password)
-                if not s.sendMail(config.receiveMail, "Veränderung im QIS", table):
-                    print("failed to sent email")
-            else:
-                print(table)
+            logger.info("Change on qis")
+            notify(diff, config)
+        else:
+            logger.info("no change")
     grades = new_grades
+    resp = session.get(logout_url)
+    if "angemeldet" in resp.text:
+        logger.warn("failed to logout")
+        logger.debug(resp.text)
     return grades
 
 def job(config):
-    print(datetime.datetime.now())
     grades = None
     ofile = 'grades.pickle'
     if os.path.isfile(ofile):
@@ -131,13 +181,9 @@ def load_config():
     return json.load(open(cfile, 'r'), object_hook=lambda d: Namespace(**d))
 
 def main():
+    setup_logging(default_level=logging.DEBUG)
     config = load_config()
     job(config)
-    schedule.every(config.schedule).hours.do(job, config=config)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
 
 
 if __name__ == "__main__":
